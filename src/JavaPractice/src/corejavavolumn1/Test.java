f4a49e72fd9ffec6a1b328e2485f5c5e58f4e41f
qizl <qizl@glodon.com>
2022-03-11 17:37:12 +0800

    添加泛型代码

--- src/JavaPractice/src/corejavavolumn1/Test.java
+++ src/JavaPractice/src/corejavavolumn1/Test.java
@@ -1,13 +1,15 @@
 package corejavavolumn1;
 
 import corejavavolumn1.chapter3.IntegerCacheTest;
-import corejavavolumn1.chapter6.ArrayAlg;
 import corejavavolumn1.chapter6.LambdaTest;
 import corejavavolumn1.chapter6.TalkingClock;
 import corejavavolumn1.chapter6.proxy.ProxyTest;
 import corejavavolumn1.chapter6.serviceLoader.Cipher;
 import corejavavolumn1.chapter6.serviceLoader.CipherLoader;
+import corejavavolumn1.chapter8.ArrayAlg;
+import corejavavolumn1.chapter8.Pair;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.ServiceLoader;
 
@@ -21,45 +23,104 @@ import java.util.ServiceLoader;
 public class Test {
     public static void main(String[] args) throws Exception {
         // chapter3
-//        var test = new SwitchTest();
-//        test.test(3);
-
-//        var test = new IntegerCacheTest();
-//        test.compareNumber();
-//        IntegerCacheTest.cache();
+        //var test = new SwitchTest();
+        //test.test(3);
+        //
+        //var test = new IntegerCacheTest();
+        //test.compareNumber();
+        //IntegerCacheTest.cache();
 
         // chapter6
-//        var test = new LambdaTest();
-//        test.testComparator();
-//        test.testListRemove();
-//        test.testFunction();
-//        test.testComparing();
-//
-//        test.getClassName();
-//        LambdaTest.getStaticClassName();
+        //var test = new LambdaTest();
+        //test.testComparator();
+        //test.testListRemove();
+        //test.testFunction();
+        //test.testComparing();
+        //
+        //test.getClassName();
+        //LambdaTest.getStaticClassName();
 
-//        var clock = new TalkingClock(1000, true);
-//        TalkingClock.TimePrinter listener = clock.new TimePrinter();
-////        clock.start();
-////        clock.innerStart();
-////        clock.anonymousStart();
-//        clock.lambdaStart();
-//
-//        var in = new Scanner(System.in);
-//        System.out.println("just wait...");
-//        var input = in.nextLine();
-//        System.out.println("exit with " + input);
-
-//        var d = new double[3];
-//        d[0] = 2;
-//        d[1] = 1;
-//        d[2] = 3;
-//        var r = ArrayAlg.minMax(d);
-
-//        CipherLoader.test();
-
-//        ProxyTest.test();
+        //var clock = new TalkingClock(1000, true);
+        //TalkingClock.TimePrinter listener = clock.new TimePrinter();
+        ////clock.start();
+        ////clock.innerStart();
+        ////clock.anonymousStart();
+        //clock.lambdaStart();
+        //
+        //var in = new Scanner(System.in);
+        //System.out.println("just wait...");
+        //var input = in.nextLine();
+        //System.out.println("exit with " + input);
+        //
+        //var d = new double[3];
+        //d[0] = 2;
+        //d[1] = 1;
+        //d[2] = 3;
+        //var r = ArrayAlg.minMax(d);
+        //
+        //CipherLoader.test();
+        //
+        //ProxyTest.test();
 
         // chapter8
+        // chapter3
+        //var test = new SwitchTest();
+        //test.test(3);
+        //
+        //var test = new IntegerCacheTest();
+        //test.compareNumber();
+        //IntegerCacheTest.cache();
+
+        // chapter6
+        //var test = new LambdaTest();
+        //test.testComparator();
+        //test.testListRemove();
+        //test.testFunction();
+        //test.testComparing();
+        //
+        //test.getClassName();
+        //LambdaTest.getStaticClassName();
+        //
+        //var clock = new TalkingClock(1000, true);
+        //TalkingClock.TimePrinter listener = clock.new TimePrinter();
+        ////clock.start();
+        ////clock.innerStart();
+        ////clock.anonymousStart();
+        //clock.lambdaStart();
+        //
+        //var in = new Scanner(System.in);
+        //System.out.println("just wait...");
+        //var input = in.nextLine();
+        //System.out.println("exit with " + input);
+        //
+        //var d = new double[3];
+        //d[0] = 2;
+        //d[1] = 1;
+        //d[2] = 3;
+        //var r = ArrayAlg.minMax(d);
+        //
+        //CipherLoader.test();
+        //
+        //ProxyTest.test();
+
+        // chapter8
+        String[] words = {"Mary", "had", "a", "little", "lamb"};
+        Pair<String> mm = ArrayAlg.minMax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+
+        //String middle = ArrayAlg.<String>getMiddle(words);
+        String middle = ArrayAlg.getMiddle(words);
+        System.out.println("middle = " + middle);
+
+        LocalDate[] birthdays = {
+                LocalDate.of(1986, 12, 9),
+                LocalDate.of(1815, 12, 10),
+                LocalDate.of(1903, 12, 3),
+                LocalDate.of(1910, 6, 22),
+        };
+        Pair<LocalDate> mm1 = ArrayAlg.minMax1(birthdays);
+        System.out.println("min = " + mm1.getFirst());
+        System.out.println("max = " + mm1.getSecond());
     }
 }
---
+++ src/JavaPractice/src/corejavavolumn1/chapter8/ArrayAlg.java
@@ -0,0 +1,39 @@
+package corejavavolumn1.chapter8;
+
+import javax.swing.text.StyledEditorKit;
+import javax.xml.transform.sax.SAXTransformerFactory;
+
+/**
+ * description: add a description
+ *
+ * @author Qizl
+ * @version 1.0.0
+ * @since 2022/03/11 09:43:03
+ */
+public class ArrayAlg {
+    public static Pair<String> minMax(String[] a) {
+        if (a == null || a.length == 0) return null;
+        String min = a[0];
+        String max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) min = a[i];
+            if (max.compareTo(a[i]) < 0) max = a[i];
+        }
+        return new Pair<>(min, max);
+    }
+
+    public static <T> T getMiddle(T... a) {
+        return a[a.length / 2];
+    }
+
+    public static <T extends Comparable> Pair<T> minMax1(T[] a) {
+        if (a == null || a.length == 0) return null;
+        T min = a[0];
+        T max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) min = a[i];
+            if (max.compareTo(a[i]) < 0) max = a[i];
+        }
+        return new Pair<>(min, max);
+    }
+}
